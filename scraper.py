b6e380c583b07b823d894eaa33f4c584f47f83e9
Patrick Little <patrick.little@tbs-sct.gc.ca>
2024-09-01 15:20:36 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,7 +4,7 @@ import csv
 
 def scrape_table(url, output_filename):
     response = requests.get(url)
-    if response.status_code == 200:  # Corrected line
+    if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         table_div = soup.find('div', class_='table-responsive2 small')
         if table_div:
@@ -21,14 +21,18 @@ def scrape_table(url, output_filename):
     else:
         print(f"Failed to retrieve {url}, status code: {response.status_code}")
 
-def scrape_html_content(url, output_filename):
+def scrape_specific_details(url, output_filename):
     response = requests.get(url)
-    if response.status_code == 200:  # Ensure this line is also correct
+    if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-        content = soup.prettify()
-        with open(output_filename, 'w') as f:
-            f.write(content)
-        print(f"Scraped and saved HTML content from {url} to {output_filename}")
+        specific_details = soup.find('h2', id='appK').find_parent('details')
+        
+        if specific_details:
+            with open(output_filename, 'w') as f:
+                f.write(specific_details.prettify())
+            print(f"Scraped and saved specific content from {url} to {output_filename}")
+        else:
+            print(f"Specific details element not found at {url}")
     else:
         print(f"Failed to retrieve {url}, status code: {response.status_code}")
 
@@ -43,12 +47,13 @@ if __name__ == "__main__":
         output_filename='docs/scraped_table_fr.csv'
     )
 
-    # Scrape the HTML content directly to the docs folder
-    scrape_html_content(
+    # Scrape the specific details for Appendix K directly to the docs folder
+    scrape_specific_details(
         url='https://www.tbs-sct.canada.ca/pol/doc-eng.aspx?id=32601#appK',
         output_filename='docs/scraped_content_eng.html'
     )
-    scrape_html_content(
+    scrape_specific_details(
         url='https://www.tbs-sct.canada.ca/pol/doc-fra.aspx?id=32601#appK',
         output_filename='docs/scraped_content_fra.html'
     )
+
